286b48863f943d54910a06114f8447655c207442
Ashraf Shaik <ashrafshaik4444@gmail.com>
2026-03-11 14:33:05 +0530

    automatedv test 1

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,27 @@
+{{ config(materialized='view') }}
+
+{%- set yaml_metadata -%}
+source_model: "{{ source('raw_data', 'raw_customers') }}"
+derived_columns:
+  RECORD_SOURCE: '!RAW_CUSTOMERS'
+  LOAD_DATE: 'CURRENT_TIMESTAMP()'
+  EFFECTIVE_FROM: 'CURRENT_TIMESTAMP()'
+hashed_columns:
+  CUSTOMER_HK: 'CUSTOMER_ID'
+  CUSTOMER_HASHDIFF:
+    is_hashdiff: true
+    columns:
+      - 'CUSTOMER_ID'
+      - 'FIRST_NAME'
+      - 'LAST_NAME'
+      - 'EMAIL'
+      - 'PHONE'
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{ automate_dv.stage(include_source_columns=true,
+                     source_model=metadata_dict['source_model'],
+                     derived_columns=metadata_dict['derived_columns'],
+                     hashed_columns=metadata_dict['hashed_columns'],
+                     ranked_columns=none) }}
---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,30 @@
+{{ config(materialized='view') }}
+
+{%- set yaml_metadata -%}
+source_model: "{{ source('raw_data', 'raw_orders') }}"
+derived_columns:
+  RECORD_SOURCE: '!RAW_ORDERS'
+  LOAD_DATE: 'CURRENT_TIMESTAMP()'
+  EFFECTIVE_FROM: 'CURRENT_TIMESTAMP()'
+hashed_columns:
+  ORDER_HK: 'ORDER_ID'
+  CUSTOMER_HK: 'CUSTOMER_ID'
+  CUSTOMER_ORDER_HK:
+    - 'CUSTOMER_ID'
+    - 'ORDER_ID'
+  ORDER_HASHDIFF:
+    is_hashdiff: true
+    columns:
+      - 'ORDER_ID'
+      - 'ORDER_DATE'
+      - 'TOTAL_AMOUNT'
+      - 'STATUS'
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{ automate_dv.stage(include_source_columns=true,
+                     source_model=metadata_dict['source_model'],
+                     derived_columns=metadata_dict['derived_columns'],
+                     hashed_columns=metadata_dict['hashed_columns'],
+                     ranked_columns=none) }}
